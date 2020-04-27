61e94394009956893863e3adde8296f0ab2ce0d4
alexdev06 <alexandre.manteaux@gmail.com>
2020-04-27 12:05:27 +0200

    Twig bundle installation

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 
 class PhoneController extends AbstractController
 {
