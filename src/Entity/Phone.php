fa978f20168ef80420a5a15741a488c6dea50ad6
alexdev06 <alexandre.manteaux@gmail.com>
2020-04-27 09:50:50 +0200

    Phone entity creation and database integration with Doctrine migration

---
+++ src/Entity/Phone.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PhoneRepository")
+ */
+class Phone
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $model;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $memory;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $color;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $network;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $intenalReference;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): self
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getMemory(): ?string
+    {
+        return $this->memory;
+    }
+
+    public function setMemory(string $memory): self
+    {
+        $this->memory = $memory;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getNetwork(): ?string
+    {
+        return $this->network;
+    }
+
+    public function setNetwork(string $network): self
+    {
+        $this->network = $network;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getIntenalReference(): ?string
+    {
+        return $this->intenalReference;
+    }
+
+    public function setIntenalReference(string $intenalReference): self
+    {
+        $this->intenalReference = $intenalReference;
+
+        return $this;
+    }
+}
