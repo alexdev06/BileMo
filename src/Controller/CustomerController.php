9deb415043cad150b213caf090c6a9291d892315
alexdev06 <alexandre.manteaux@gmail.com>
2020-04-29 15:28:35 +0200

    List method has been created in CustomerController class

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -2,10 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\CustomerRepository;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 
 class CustomerController extends AbstractController
 {
+    /**
+     * @Rest\Get(
+     *      path = "/customers",
+     *      name = "customer_list"
+     * )
+     * @Rest\View(statusCode = 200)
+     */
+    public function list(CustomerRepository $customerRepository)
+    {
+        return $customerRepository->findAll();
+    }
 
 }
