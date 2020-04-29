03567d706dad800f3a7bbdaa2d12db92334114d6
alexdev06 <alexandre.manteaux@gmail.com>
2020-04-29 18:42:54 +0200

    Validation constraints have been added in Client entity class

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -6,9 +6,17 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ClientRepository")
+ * @UniqueEntity(
+ *      fields={"username"}),
+ *      message="The username is unavailable!"
+ * )
  */
 class Client implements UserInterface
 {
@@ -21,6 +29,7 @@ class Client implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @Assert\Length(min=2)
      */
     private $username;
 
@@ -32,16 +41,19 @@ class Client implements UserInterface
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
+     * @Assert\NotBlank()
      */
     private $password;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
      */
     private $firstname;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
      */
     private $lastname;
 
@@ -52,11 +64,13 @@ class Client implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Email(message = "The email value is not a valid email !")
      */
     private $email;
 
     /**
      * @ORM\Column(type="datetime")
+     * @Assert\DateTime()
      */
     private $registeredAt;
 
